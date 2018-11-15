628fd4ce9107f317a4cab6b2b770d7a1b0c97e55
Yogita Garud <garudyogitap@gmail.com>
2018-11-15 09:23:51 -0800

    graphics file

---
+++ graphics.py
@@ -0,0 +1,22 @@
+import Tkinter
+
+class HangManCanvas:
+    SCAFFOLD_HEIGHT = 360
+    BEAM_LENGTH = 144
+    ROP_LENGTH = 18
+    HEAD_RADIUS = 36
+    BODY_LENGTH = 144
+    ARM_OFFSET_FROM_HEAD = 28
+    UPPER_ARM_LENGTH = 72
+    LOWER_ARM_LENGTH = 44
+    HIP_WIDTH = 36
+    LEG_LENGTH = 108
+    FOOT_LENGTH = 28
+
+    def reset():
+
+    def displayWord(word):
+
+    def noteIncorrectGuess(letter):
+
+
